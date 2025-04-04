dc1b1cd5b0694c3b06069f0844641e5801efbb77
YoonhaHong <yoonha@pusan.ac.kr>
2025-04-04 15:21:52 +0900

    Draw by THR/VCASB

--- plots/draw_eff+fhr.py
+++ plots/draw_eff+fhr.py
@@ -11,7 +11,6 @@ from datetime import date, datetime
 from drawing import *
 
 
-
 def extract_value_from_dir(base_dir, output_dir):
     """
     Analyze and collect efficiencies for all detectors.
@@ -36,31 +35,56 @@ def extract_value_from_dir(base_dir, output_dir):
                 root_file_path = os.path.join(detector_path, file)
                 match = re.search(r'VCASB(\d+)', file)
                 VCASB = int(match.group(1)) if match else 1
-                data.append({"Detector": detector, "VCASB": VCASB} | extract_values(root_file_path, dut_name, output_dir) )
+                data.append({"Detector": detector, "VCASB": VCASB} | extract_values(root_file_path, dut_name, output_dir))
     
     return pd.DataFrame(data)
 
+
 if __name__ == "__main__":
-    # Base directory containing batch_analysis folders
+    # Argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument("base_dir", type=str)
     parser.add_argument("-o", "--output_dir", type=str, required=False, default=".")
+    parser.add_argument("-t", "--thr_csv", type=str, default="./THR_summary.csv", help="Path to THR_summary.csv")
+    parser.add_argument("-f", "--fhr_csv", type=str, default="./FHR_summary.csv", help="Path to FHR_summary.csv")
+
+    xscale = input("Plot as a function of THR or VCASB? (Type THR/VCASB)    ")
+    if not xscale in ["THR", "VCASB"]: quit()
+    elif xscale == "THR":
+        xname = "ThresholdAverage"
+    elif xscale == "VCASB":
+        xname = xscale
 
     args = parser.parse_args()
-    base_directory = args.base_dir[:-1] if args.base_dir.endswith("/") else args.base_dir # delete "/"
+    base_directory = args.base_dir.rstrip("/")  # Remove trailing "/"
     output_dir = os.path.join(args.output_dir, os.path.basename(base_directory))
     os.makedirs(output_dir, exist_ok=True)
 
-    csv_path = f"{output_dir}/{os.path.basename(base_directory)}+efficiency_results.csv" 
+    csv_path = f"{output_dir}/{os.path.basename(base_directory)}+efficiency_results.csv"
     if os.path.exists(csv_path): 
         print(f"Skipping extracting values, {csv_path} detected!")
         efficiency_df = pd.read_csv(csv_path)
     else:   
         efficiency_df = extract_value_from_dir(base_directory, output_dir)
         efficiency_df.to_csv(csv_path, index=False)
-    efficiency_df = efficiency_df.sort_values('Detector')
-    print(efficiency_df.head(5))
     
+    efficiency_df = efficiency_df.sort_values('Detector')
+
+    thr_df = pd.read_csv(args.thr_csv)
+    # Add THR values
+    efficiency_df = efficiency_df.merge(
+    thr_df[['Detector', 'VCASB', 'ThresholdAverage', 'ThresholdRMS']],
+        on=['Detector', 'VCASB'],
+        how='left')
+    
+    fhr_df = pd.read_csv(args.fhr_csv)
+    # Add FHR values
+    efficiency_df = efficiency_df.merge(
+    fhr_df[['Detector', 'VCASB', 'MaskedFakeHitRate','MaskedFakeHitRateErrorUp','MaskedFakeHitRateErrorLow']],
+        on=['Detector', 'VCASB'],
+        how='left')
+
+    print(efficiency_df.head(5))
 
     # Apply WP3 style settings
     plt.rcParams['figure.subplot.bottom'] = 0.13
@@ -83,17 +107,19 @@ if __name__ == "__main__":
     plt.subplots_adjust(left=0.08, right=0.70, bottom=0.10, top=0.95)
 
     for detector in efficiency_df['Detector'].unique():
-        detector_data = efficiency_df[efficiency_df['Detector'] == detector]
-        detector_data = detector_data.sort_values('VCASB')  # VCASB 순서대로 정렬
-        ax1.errorbar(detector_data['VCASB'], 
-                detector_data['efficiency']*100., 
-                yerr=[detector_data['efficiency_errorlow']*100., detector_data['efficiency_errorup']*100.],
-                fmt='o', linestyle='-', 
-                label=f"{detector}")
-    #ax1.legend(bbox_to_anchor=(1.15, 0), loc='lower left', borderaxespad=0.)
-    
+        detector_data = efficiency_df[efficiency_df['Detector'] == detector].sort_values(xname)
+        ax1.errorbar(detector_data[xname], 
+                     detector_data['efficiency'] * 100., 
+                     yerr=[detector_data['efficiency_errorlow'] * 100., detector_data['efficiency_errorup'] * 100.],
+                     fmt='o', linestyle='-', 
+                     label=f"{detector}")
+
     ax1.grid()
-    ax1.set_xlabel(r'$\it{V_{casb}}$ [DAC]')
+    if xscale == "THR":
+        ax1.set_xlabel('Threshold [DAC]')
+        ax1.set_xlim(10, 40)
+    else:
+        ax1.set_xlabel(r'$\it{V_{casb}}$ [DAC]')
     ax1.set_ylabel('Detection Efficiency [%]')
 
     ax1.tick_params(axis='y')
@@ -101,28 +127,18 @@ if __name__ == "__main__":
     ax1.set_yticks(np.linspace(ax1.get_ybound()[0], ax1.get_ybound()[1], 11)) 
 
     ax1.axhline(y=99.0, color='grey', linestyle='--')
-    
-    #print(ax1.get_xlim()[0])
-    #print(ax1.get_xlim()[1])
-    #print(ax1.get_xlim()[0]-0.014*(ax1.get_xlim()[1]-ax1.get_xlim()[0]))
-    #ax1.text(ax1.get_xlim()[0]-0.014*(ax1.get_xlim()[1]-ax1.get_xlim()[0]), 99, "99", fontsize=12, color='grey', ha='right', va='center')
-    #ax1.text(ax1.get_xlim()[0]*1.05, 99*1.00, "99", fontsize=12, color='grey', ha='right', va='center') ## ongoing 
 
-
-        
-    # 두 번째 y축 생성 (twinx를 사용해 ax1과 x축을 공유)
     ax2 = ax1.twinx()
     ax2.errorbar([], [], ([], []), label="Detection efficiency", marker='s', linestyle='-', elinewidth=1.3, capsize=1.5, color='dimgrey')
     ax2.errorbar([], [], ([], []), label="Fake-hit rate", marker='s', markerfacecolor='none', linestyle='--', elinewidth=1.3, capsize=1.5, color='dimgrey')
+
     for detector in efficiency_df['Detector'].unique():
-        detector_data = efficiency_df[efficiency_df['Detector'] == detector]
-        detector_data = detector_data.sort_values('VCASB')  # VCASB 순서대로 정렬
-        ax2.errorbar(detector_data['VCASB'], 
-                np.zeros(len(detector_data)), 
-                yerr=np.zeros(len(detector_data)),
-                fmt='o', linestyle='--', mfc='none',
-                label=f"{detector}")
-    
+        detector_data = efficiency_df[efficiency_df['Detector'] == detector].sort_values(xname)
+        ax2.errorbar(detector_data[xname], 
+                     detector_data['MaskedFakeHitRate'],
+                     yerr=[detector_data['MaskedFakeHitRateErrorLow'], detector_data['MaskedFakeHitRateErrorUp']],
+                     fmt='o', linestyle='--', mfc='none', label=f"{detector}")
+
     ax2.set_ylabel('Fake-hit rate [hits/pixel/event]')
     ax2.tick_params(axis='y')  
     ax2.set_ylim(0, 1e-1) #era -3
@@ -132,7 +148,8 @@ if __name__ == "__main__":
     ax2.axhline(y=1e-6, color='grey', linestyle=':')
     ax2.text(ax2.get_xlim()[1]*0.99, 1e-6*1.70, "FHR measurement sensitivity limit", fontsize=9, color='grey', ha='right', va='top')
     ax2.legend(bbox_to_anchor=(1.15, 0), loc='lower left', borderaxespad=0.)
-  
+
+    ax2.legend(bbox_to_anchor=(1.15, 0), loc='lower left', borderaxespad=0.)
     lines = Get_descriptions(dir_name=base_directory)
     multiline_text_legend = "\n".join(lines)
     #mask_line = Get_mask_info(Halfunit, CHIPNAME)
@@ -145,10 +162,11 @@ if __name__ == "__main__":
     ]
     multiline_text_beam = "\n".join(lines_beam)
 
+
     fig.text(1.13, 0.97, "babyMOSS-2_4_W21D4", fontweight='bold', transform=ax1.transAxes)
     fig.text(1.13, 0.94, multiline_text_legend.strip(), verticalalignment='top', horizontalalignment='left', transform=ax1.transAxes)
-    fig.text(0.01, 0.955, multiline_text_beam.strip(), fontsize=13, ha='left', va='top',transform=ax1.transAxes)
+    fig.text(0.7, 0.955, multiline_text_beam.strip(), fontsize=13, ha='left', va='top',transform=ax1.transAxes)
 
-    plt.savefig(f"{output_dir}/{os.path.basename(base_directory)}+efficiency_vs_VCASB.png")
+    plt.savefig(f"{output_dir}/{os.path.basename(base_directory)}+effandfhr_vs_{xscale}.png")
     if platform.system() == "Darwin":  # macOS
        plt.show() 
--- plots/draw_res+cs.py
+++ plots/draw_res+cs.py
@@ -1,3 +1,7 @@
+'''
+Drawing Spatial-Resolution & Cluster-Size as a function of VCASB
+by Yoonha Hong
+'''
 import sys
 import os
 import platform
@@ -45,6 +49,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("base_dir", type=str)
     parser.add_argument("-o", "--output_dir", type=str, required=False, default=".")
+    parser.add_argument("-t", "--thr_csv", type=str, default="./THR_summary.csv", help="Path to THR_summary.csv")
+    parser.add_argument("-f", "--fhr_csv", type=str, default="./FHR_summary.csv", help="Path to FHR_summary.csv")
+    
+    xscale = input("Plot as a function of THR or VCASB? (Type THR/VCASB)    ")
+    if not xscale in ["THR", "VCASB"]: quit()
+    elif xscale == "THR":
+        xname = "ThresholdAverage"
+    elif xscale == "VCASB":
+        xname = xscale
+
 
     args = parser.parse_args()
     base_directory = args.base_dir[:-1] if args.base_dir.endswith("/") else args.base_dir # delete "/"
@@ -59,6 +73,21 @@ if __name__ == "__main__":
         efficiency_df = extract_value_from_dir(base_directory, output_dir)
         efficiency_df.to_csv(csv_path, index=False)
     efficiency_df = efficiency_df.sort_values('Detector')
+    
+    thr_df = pd.read_csv(args.thr_csv)
+    # Add THR values
+    efficiency_df = efficiency_df.merge(
+    thr_df[['Detector', 'VCASB', 'ThresholdAverage', 'ThresholdRMS']],
+        on=['Detector', 'VCASB'],
+        how='left')
+    
+    fhr_df = pd.read_csv(args.fhr_csv)
+    # Add FHR values
+    efficiency_df = efficiency_df.merge(
+    fhr_df[['Detector', 'VCASB', 'MaskedFakeHitRate','MaskedFakeHitRateErrorUp','MaskedFakeHitRateErrorLow']],
+        on=['Detector', 'VCASB'],
+        how='left')
+
     print(efficiency_df.head(5))
     
 
@@ -85,54 +114,49 @@ if __name__ == "__main__":
     for detector in efficiency_df['Detector'].unique():
         detector_data = efficiency_df[efficiency_df['Detector'] == detector]
         detector_data = detector_data.sort_values('VCASB')  # VCASB 순서대로 정렬
-        ax1.errorbar(detector_data['VCASB'], 
-                detector_data['efficiency']*100., 
-                yerr=[detector_data['efficiency_errorlow']*100., detector_data['efficiency_errorup']*100.],
-                fmt='o', linestyle='-', 
+        ax1.errorbar(detector_data[xname], 
+                detector_data['spatialres'], 
+                yerr=detector_data['spatialres_error'],
+                fmt='o', linestyle='-',
                 label=f"{detector}")
-    #ax1.legend(bbox_to_anchor=(1.15, 0), loc='lower left', borderaxespad=0.)
-    
     ax1.grid()
-    ax1.set_xlabel(r'$\it{V_{casb}}$ [DAC]')
-    ax1.set_ylabel('Detection Efficiency [%]')
+    if xscale == "THR":
+        ax1.set_xlabel('Threshold [DAC]')
+        ax1.set_xlim(10, 40)
+    else:
+        ax1.set_xlabel(r'$\it{V_{casb}}$ [DAC]')
 
+    ax1.set_ylabel('Spatial resolution [\u03BCm]')
     ax1.tick_params(axis='y')
-    ax1.set_ylim(91,101)
-    ax1.set_yticks(np.linspace(ax1.get_ybound()[0], ax1.get_ybound()[1], 11)) 
-
-    ax1.axhline(y=99.0, color='grey', linestyle='--')
-    
-    #print(ax1.get_xlim()[0])
-    #print(ax1.get_xlim()[1])
-    #print(ax1.get_xlim()[0]-0.014*(ax1.get_xlim()[1]-ax1.get_xlim()[0]))
-    #ax1.text(ax1.get_xlim()[0]-0.014*(ax1.get_xlim()[1]-ax1.get_xlim()[0]), 99, "99", fontsize=12, color='grey', ha='right', va='center')
-    #ax1.text(ax1.get_xlim()[0]*1.05, 99*1.00, "99", fontsize=12, color='grey', ha='right', va='center') ## ongoing 
 
 
-        
+    if "tb" in base_directory:
+        pitch = 22.5 / np.sqrt(12)
+    elif "bb" in base_directory:
+        pitch = 18.0 / np.sqrt(12)
+    ax1.axhline(y=pitch, color='grey', linestyle=':')
+    ax1.text(ax1.get_xlim()[0]*1.05, pitch, r"Pixel Pitch/$\sqrt{12}", fontsize=12, color='grey', ha='right', va='center') ## ongoing 
+
     # 두 번째 y축 생성 (twinx를 사용해 ax1과 x축을 공유)
     ax2 = ax1.twinx()
-    ax2.errorbar([], [], ([], []), label="Detection efficiency", marker='s', linestyle='-', elinewidth=1.3, capsize=1.5, color='dimgrey')
-    ax2.errorbar([], [], ([], []), label="Fake-hit rate", marker='s', markerfacecolor='none', linestyle='--', elinewidth=1.3, capsize=1.5, color='dimgrey')
+    ax2.errorbar([], [], ([], []), label="Spatial resolution", marker='s', linestyle='-', elinewidth=1.3, capsize=1.5, color='dimgrey')
+    ax2.errorbar([], [], ([], []), label="Cluster size", marker='s', markerfacecolor='none', linestyle='--', elinewidth=1.3, capsize=1.5, color='dimgrey')
     for detector in efficiency_df['Detector'].unique():
         detector_data = efficiency_df[efficiency_df['Detector'] == detector]
         detector_data = detector_data.sort_values('VCASB')  # VCASB 순서대로 정렬
-        ax2.errorbar(detector_data['VCASB'], 
-                np.zeros(len(detector_data)), 
-                yerr=np.zeros(len(detector_data)),
-                fmt='o', linestyle='--', mfc='none',
+        ax2.errorbar(detector_data[xname], 
+                detector_data['clustersize'], 
+                yerr=detector_data['clustersize_error'],
+                fmt='o',  linestyle='--', mfc='none',
                 label=f"{detector}")
-    
-    ax2.set_ylabel('Fake-hit rate [hits/pixel/event]')
-    ax2.tick_params(axis='y')  
-    ax2.set_ylim(0, 1e-1) #era -3
-    ax2.set_yscale('symlog',linthresh=1e-10, linscale=0.90)
-    ax2.set_yticks([0, 1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1])
-
-    ax2.axhline(y=1e-6, color='grey', linestyle=':')
-    ax2.text(ax2.get_xlim()[1]*0.99, 1e-6*1.70, "FHR measurement sensitivity limit", fontsize=9, color='grey', ha='right', va='top')
+    ax2.set_ylabel('Average cluster size [pixels]')
+    ax2.tick_params(axis='y') 
+    # ax2.set_ylim(91,101) 
+    # ax1.set_ylim(91,101)
+    # ax1.set_xlim(45,130)
+    # ax1.axhline(y=99.0, color='grey', linestyle='--')
     ax2.legend(bbox_to_anchor=(1.15, 0), loc='lower left', borderaxespad=0.)
-  
+
     lines = Get_descriptions(dir_name=base_directory)
     multiline_text_legend = "\n".join(lines)
     #mask_line = Get_mask_info(Halfunit, CHIPNAME)
@@ -148,7 +172,7 @@ if __name__ == "__main__":
     fig.text(1.13, 0.97, "babyMOSS-2_4_W21D4", fontweight='bold', transform=ax1.transAxes)
     fig.text(1.13, 0.94, multiline_text_legend.strip(), verticalalignment='top', horizontalalignment='left', transform=ax1.transAxes)
     fig.text(0.01, 0.955, multiline_text_beam.strip(), fontsize=13, ha='left', va='top',transform=ax1.transAxes)
-
-    plt.savefig(f"{output_dir}/{os.path.basename(base_directory)}+efficiency_vs_VCASB.png")
+    #fig.text(0.01, 0.025, f'{mask_line}', fontsize=9, color='black', ha='left', va='center', transform=ax1.transAxes)
+    plt.savefig(f"{output_dir}/{os.path.basename(base_directory)}+resolandclust_vs_{xscale}.png")
     if platform.system() == "Darwin":  # macOS
        plt.show() 
